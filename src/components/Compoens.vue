6936ee281faefadf63b6e411193cc77b0f0c575f
xiaohe <1794713974@qq.com>
2022-08-19 17:34:48 +0800

    fix:编辑器左右两边超高部分改为滚动

--- src/components/Compoens.vue
+++ src/components/Compoens.vue
@@ -1,92 +1,89 @@
 <template>
-    <div class="components-box">
-      <div class="header-logo">
-        CEditor
-      </div>
-      <div class="container">
-        <div class="container-header">组件库</div>
-            <el-collapse>
-                <el-collapse-item title="基础组件" name="1">
-                  <div
-                    v-for="item in cList.filter(v => v.type === 'normal')"
-                    :key="item.name"
-                    class="component-card"
-                    draggable="true"
-                    @dragstart="(e) => handleDragStart(e, item)"
-                  >
-                    <div class="card-top"
-                      :style="{
-                        width: '100%',
-                        height: '100%',
-                        background: `url(${item.imgSrc})`,
-                        backgroundSize: '40%',
-                        backgroundRepeat: 'no-repeat',
-                        backgroundPosition: 'center',
-                      }"
-                    >
-                    </div>
-                    <div class="card-bottom">
-                      {{ item.label }}
-                    </div>
-                  </div>
-                </el-collapse-item>
-                <el-collapse-item title="数据展示组件" name="2">
-                  <div
-                    v-for="item in cList.filter(v => v.type === 'chart')"
-                    :key="item.name"
-                    class="component-card"
-                    draggable="true"
-                    @dragstart="(e) => handleDragStart(e, item)"
-                  >
-                    <div class="card-top"
-                      :style="{
-                        width: '100%',
-                        height: '100%',
-                        background: `url(${item.imgSrc})`,
-                        backgroundSize: 'contain',
-                        backgroundRepeat: 'no-repeat',
-                        backgroundPosition: 'center',
-                      }"
-                    >
-                    </div>
-                    <div class="card-bottom">
-                      {{ item.label }}
-                    </div>
-                  </div>
-                </el-collapse-item>
-                <el-collapse-item title="特色组件" name="3">
-                  <div
-                    v-for="item in cList.filter(v => v.type === 'custom')"
-                    :key="item.name"
-                    class="component-card"
-                    draggable="true"
-                    @dragstart="(e) => handleDragStart(e, item)"
-                  >
-                    <div class="card-top"
-                      :style="{
-                        width: '100%',
-                        height: '100%',
-                        background: `url(${item.imgSrc})`,
-                        backgroundSize: 'contain',
-                        backgroundRepeat: 'no-repeat',
-                        backgroundPosition: 'center',
-                      }"
-                    >
-                    </div>
-                    <div class="card-bottom">
-                      {{ item.label }}
-                    </div>
-                  </div>
-                </el-collapse-item>
-            </el-collapse>
-      </div>
+  <div class="components-box">
+    <div class="header-logo">CEditor</div>
+    <div class="container">
+      <div class="container-header">组件库</div>
+      <el-collapse>
+        <el-collapse-item title="基础组件" name="1">
+          <div
+            v-for="item in cList.filter((v) => v.type === 'normal')"
+            :key="item.name"
+            class="component-card"
+            draggable="true"
+            @dragstart="(e) => handleDragStart(e, item)"
+          >
+            <div
+              class="card-top"
+              :style="{
+                width: '100%',
+                height: '100%',
+                background: `url(${item.imgSrc})`,
+                backgroundSize: '40%',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+              }"
+            ></div>
+            <div class="card-bottom">
+              {{ item.label }}
+            </div>
+          </div>
+        </el-collapse-item>
+        <el-collapse-item title="数据展示组件" name="2">
+          <div
+            v-for="item in cList.filter((v) => v.type === 'chart')"
+            :key="item.name"
+            class="component-card"
+            draggable="true"
+            @dragstart="(e) => handleDragStart(e, item)"
+          >
+            <div
+              class="card-top"
+              :style="{
+                width: '100%',
+                height: '100%',
+                background: `url(${item.imgSrc})`,
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+              }"
+            ></div>
+            <div class="card-bottom">
+              {{ item.label }}
+            </div>
+          </div>
+        </el-collapse-item>
+        <el-collapse-item title="特色组件" name="3">
+          <div
+            v-for="item in cList.filter((v) => v.type === 'custom')"
+            :key="item.name"
+            class="component-card"
+            draggable="true"
+            @dragstart="(e) => handleDragStart(e, item)"
+          >
+            <div
+              class="card-top"
+              :style="{
+                width: '100%',
+                height: '100%',
+                background: `url(${item.imgSrc})`,
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+              }"
+            ></div>
+            <div class="card-bottom">
+              {{ item.label }}
+            </div>
+          </div>
+        </el-collapse-item>
+      </el-collapse>
     </div>
+  </div>
 </template>
 <script>
 export default {
   props: {},
-  components: {
-  },
+  components: {},
   data() {
     return {
       cList: [
@@ -118,25 +115,29 @@ export default {
           name: 'bar',
           label: '柱状图',
           type: 'chart',
-          imgSrc: 'http://rb6rk8283.hd-bkt.clouddn.com/editor/bar-simple.webp?e=1651381479&token=9rPdsMX3D4ANa5yaN6J-PsinXDmd6xMP185zAzTo:JNm6-OFTeGYyDoOMdjAeRSAo_30=',
+          imgSrc:
+            'http://rb6rk8283.hd-bkt.clouddn.com/editor/bar-simple.webp?e=1651381479&token=9rPdsMX3D4ANa5yaN6J-PsinXDmd6xMP185zAzTo:JNm6-OFTeGYyDoOMdjAeRSAo_30=',
         },
         {
           name: 'line',
           label: '折线图',
           type: 'chart',
-          imgSrc: 'http://rb6rk8283.hd-bkt.clouddn.com/editor/line-simple.webp?e=1651381479&token=9rPdsMX3D4ANa5yaN6J-PsinXDmd6xMP185zAzTo:OqfljJSOl7FGDsvPFvdIh9eGEHA=',
+          imgSrc:
+            'http://rb6rk8283.hd-bkt.clouddn.com/editor/line-simple.webp?e=1651381479&token=9rPdsMX3D4ANa5yaN6J-PsinXDmd6xMP185zAzTo:OqfljJSOl7FGDsvPFvdIh9eGEHA=',
         },
         {
           name: 'pie',
           label: '饼图',
           type: 'chart',
-          imgSrc: 'http://rb6rk8283.hd-bkt.clouddn.com/editor/pie-simple.webp?e=1651381479&token=9rPdsMX3D4ANa5yaN6J-PsinXDmd6xMP185zAzTo:Ye_nMzrKbiXumZlZjkUBRQHTdXQ=',
+          imgSrc:
+            'http://rb6rk8283.hd-bkt.clouddn.com/editor/pie-simple.webp?e=1651381479&token=9rPdsMX3D4ANa5yaN6J-PsinXDmd6xMP185zAzTo:Ye_nMzrKbiXumZlZjkUBRQHTdXQ=',
         },
         {
           name: 'area',
           label: '面积图',
           type: 'chart',
-          imgSrc: 'http://rb6rk8283.hd-bkt.clouddn.com/editor/area-basic.webp?e=1651381479&token=9rPdsMX3D4ANa5yaN6J-PsinXDmd6xMP185zAzTo:NTp4Bm2jA4-3duGQSeWZkBf7p7k=',
+          imgSrc:
+            'http://rb6rk8283.hd-bkt.clouddn.com/editor/area-basic.webp?e=1651381479&token=9rPdsMX3D4ANa5yaN6J-PsinXDmd6xMP185zAzTo:NTp4Bm2jA4-3duGQSeWZkBf7p7k=',
         },
         {
           name: 'funnel',
@@ -176,9 +177,15 @@ export default {
 .components-box {
   width: 355px;
   box-shadow: 4px 0px 6px rgba(221, 221, 221, 0.726);
+
+  overflow-x: hidden;
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
   .header-logo {
     padding: 20px;
-    border-bottom: .02666667rem solid #ebedf0;
+    border-bottom: 0.02666667rem solid #ebedf0;
   }
   .container {
     .container-header {
@@ -215,9 +222,8 @@ export default {
       box-shadow: 0 0 5px 0 #dcdade;
       border-radius: 4px;
       &:hover {
-        box-shadow: 0 1px 2px -2px rgba(0, 0, 0, 16%),
-        0 3px 6px 0 rgba(0, 0, 0, 12%),
-        0 5px 12px 4px rgba(0, 0, 0, 9%);
+        box-shadow: 0 1px 2px -2px rgba(0, 0, 0, 16%), 0 3px 6px 0 rgba(0, 0, 0, 12%),
+          0 5px 12px 4px rgba(0, 0, 0, 9%);
         .card-top {
           transform: scale(1.2);
         }
@@ -228,8 +234,7 @@ export default {
       .card-top {
         font-size: 15px;
         color: #000;
-        font-family:'Gill Sans', 'Gill Sans MT',
-        Calibri, 'Trebuchet MS', sans-serif;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
         transition: 0.2s linear;
       }
       .card-bottom {
